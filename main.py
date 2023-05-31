71fcc5c7e30b0003c562117445d386faa3c2fa93
Timerlan Chinchaev <71828100+tamirlan1919@users.noreply.github.com>
2023-05-31 20:06:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -163,5 +163,5 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True,host = "0.0.0.0")
+    app.run(debug=True,host = "0.0.0.0",port = 5000)
     
